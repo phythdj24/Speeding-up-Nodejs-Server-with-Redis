ed1520e721b042c6fc3529cbc2d8e2c7b8eba5e5
ATHARV DALAL <sangamdalal24@gmail.com>
2024-03-02 22:46:14 +0530

    Updated

--- string.js
+++ string.js
@@ -2,7 +2,7 @@ import client from "./client.js";
 
 const init = async()=>{
 
-    // await client.set('msg:4', 'hey from node js')
+    await client.set('msg:4', 'hey from node js')
       const result = await client.get('msg:4');
       console.log("Result =>" ,result);
 }
